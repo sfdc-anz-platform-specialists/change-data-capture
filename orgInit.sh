bea6d2c9c0e253c28c169e43cf0dee59c2c146c8
sfdc-anz-platform-specialists <62044708+sfdc-anz-platform-specialists@users.noreply.github.com>
2020-04-17 15:03:47 +1000

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 5 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 7 -s
 sfdx force:source:push
 sfdx force:user:password:generate
 sfdx force:org:open
